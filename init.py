077f173d3f7fbbe25f6622c17bbc3caec574bc9a
Sam Fogarty <samuel.fogarty@gmail.com>
2024-03-18 13:36:02 -0600

    Fix bug, also hardcode VGAIN and offset in script for now

--- init.py
+++ init.py
@@ -40,14 +40,17 @@ def write_and_read(thing, CmdString, get_response=True):
         ResString = ResString + chr(0)
         #print(ResString)
 
-def main(EP, offset, VGAIN):
+def main(EP):
 
-    channels = [0,1,2,3,4,5,6,7]
+    VGAIN = 0
+    offset = 1118
+
     AFEs = [0,1,2,3,4]
     IP_endpoints = [ep for ep in EP.split(',')]
+    IP_start = "10.73.137"
 
     for i in IP_endpoints:
-        thing = OEI(f"10.73.137.{i}")
+        thing = OEI(f"{IP_start}.{i}")
         if False:
             write_and_read(thing, 'RD OFFSET CH 6', get_response=True)
 
@@ -56,10 +59,9 @@ def main(EP, offset, VGAIN):
             write_and_read(thing, 'CFG AFE ALL INITIAL')
 
             # write commands to each channel
-            for AFE in AFEs:
-                for ch in tqdm(channels,unit='Channels'):
-                    write_and_read(thing, 'WR OFFSET CH ' + str(int(AFE*ch)) + f' V {offset}', get_response=True)
-                    write_and_read(thing, 'CFG OFFSET CH ' + str(int(AFE*ch)) + ' GAIN 2', get_response=True)
+            for ch in tqdm(range(40),unit='Channels'):
+                write_and_read(thing, 'WR OFFSET CH ' + str(int(ch)) + f' V {offset}', get_response=True)
+                write_and_read(thing, 'CFG OFFSET CH ' + str(int(ch)) + ' GAIN 2', get_response=True)
 
             print(f'Configuring AFE registers 4, 51, 52 and Attenuators')
             # write commands to each AFE
@@ -73,11 +75,10 @@ def main(EP, offset, VGAIN):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script for configuring DAPHNE analog front end")
-    parser.add_argument('--ep', required=True, help='DAPHNE IP endpoint (assuming IP starts with 10.73.137.')
-    parser.add_argument('--offset', required=False, default=1118, help='Channel offset DAC value for all channels')
-    parser.add_argument('--vgain', required=False, default=600, help='VGAIN DAC setting to control gain/attenuation in DAPHNE')
+    parser.add_argument('--ep', required=True, help='DAPHNE IP endpoint (assuming IP starts with 10.73.137., otherwise edit script)')
+    #parser.add_argument('--vgain', required=False, default=None, help='VGAIN DAC setting to control gain/attenuation in DAPHNE')
     args = parser.parse_args()
-    main(args.ep, args.offset, args.vgain)
+    main(args.ep)
 
 
 
