f1b6b505db6307c956bde891ca5ae1f01b024ec1
Sam Fogarty <samuel.fogarty@colostate.edu>
2024-06-20 11:53:12 -0500

    Plot also amplitude distribution

--- analysis/persistence.py
+++ analysis/persistence.py
@@ -8,9 +8,9 @@ import pandas as pd
 
 def main(filename):
     wvfms = np.genfromtxt(filename, delimiter=' ', max_rows=10000)
-    window_size = 20
+    window_size = 1
     pedestal_range = (350, 450)
-
+    amplitudes = []
     # make array to store smoothed waveforms
     wvfm_corr = np.zeros((wvfms.shape[0], wvfms.shape[1]))
     for eventID in tqdm(range(wvfm_corr.shape[0])):
@@ -24,13 +24,14 @@ def main(filename):
         # Subtract pedestal
     
         wvfm_smoothed = wvfm_smoothed - mean
+        amplitudes.append(np.min(wvfm_smoothed))
         wvfm_corr[eventID, :] = wvfm_smoothed
     num_waveforms, num_time_steps = wvfm_corr.shape
     #num_time_steps -= window_size
     flattened_waveforms = wvfm_corr.ravel()
     #flattened_waveforms = flattened_waveforms[~np.isnan(flattened_waveforms)]
 
-    plt.figure(figsize=(10, 6))
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(16,6))
     meanADC = np.mean(flattened_waveforms[~np.isnan(flattened_waveforms)])
     # Adjust the number of bins and range
     ymax = 10
@@ -39,28 +40,33 @@ def main(filename):
     num_bins_y = int((ymax - ymin)/1)
     range_x = [0, num_time_steps*16]
     range_y = [ymin, ymax]
-
-    plt.hist2d(
+    
+    plot2d = axes[0].hist2d(
             np.tile(np.arange(num_time_steps)*16, num_waveforms),
             flattened_waveforms,
             bins=[num_bins_x, num_bins_y],
             range=[range_x, range_y],
             cmap='viridis',
-            cmin=1
+            vmin=1,
+            vmax=700
             )
-
-    plt.xlabel(r'Time [ns]')
-    plt.ylabel('Amplitude (ADC counts)')
+    #cmap = plt.cm.jet    
+    axes[0].set_xlabel(r'Time [ns]')
+    axes[0].set_ylabel('Amplitude (ADC counts)')
     #plt.title(f'Persistence Plot (Run: {runnumber}, LED Voltage: {runnum_LED_dict[runnumber]}V)')
-    cbar = plt.colorbar()
+    colorbar = fig.colorbar(plot2d[3], ax=axes[0])
 
     vmin=0
     vmax=500 # may need to adjust to better emphasize N p.e. waveform lines
-    plt.clim(vmin=vmin, vmax=vmax)
+    #colorbar.clim(vmin=vmin, vmax=vmax)
     
     #plt.ylim(-40, 10)
-    plt.xlim(9000, 12000)
+    axes[0].set_xlim(9000, 11000)
     #plt.ylim(-50, 30)
+
+    axes[1].hist(amplitudes, bins=75, range=(-75, 10))
+    axes[1].set_xlabel('Waveform Amplitude')
+    axes[1].set_xticks([-70,-60,-50,-40,-30,-20,-10,0, 10])
     plt.show()
     
 
