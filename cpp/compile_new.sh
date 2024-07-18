7d3d2a1b8c57ac2b3492531defd4986b5fedc7db
Noah Vardy <noahvardy4@gmail.com>
2024-07-17 20:35:34 -0400

    Compiles collect_data_new.cpp

---
+++ cpp/compile_new.sh
@@ -0,0 +1,4 @@
+g++ -c daphne.cpp -o daphne.o
+ar rcs libdaphne.a daphne.o
+#g++ plot_data.cpp daphne.cpp -o plot_data -I$ROOTSYS/include -L$ROOTSYS/lib -lCore -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lGui -L. -ldaphnei
+g++ collect_data_new.cpp daphne.cpp -o collect_data_new -ldaphne -lhdf5_cpp -lhdf5 #-I$ROOTSYS/include -L$ROOTSYS/lib -lCore -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lGui -L. -ldaphne
