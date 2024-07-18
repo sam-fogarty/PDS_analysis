002582dc16fed6418033d8d96e4838616b6f5478
Noah Vardy <noahvardy4@gmail.com>
2024-07-18 19:07:50 -0400

    This version can handle both .csv and .hdf5 files

--- analysis/DCR.py
+++ analysis/DCR.py
@@ -4,6 +4,7 @@ import argparse
 from scipy.signal import butter, filtfilt
 from tqdm import tqdm
 from scipy.signal import correlate
+import h5py
 
 def apply_filter(waveform, cutoff_freq, order):
     """
@@ -31,7 +32,14 @@ def main(filepath):
     # load data
     skiprows=0
     print(f'Loading waveforms from {filepath}')
-    wvfms_orig = np.loadtxt(filepath, delimiter=' ', skiprows=skiprows)
+    if '.csv' in filepath:
+        wvfms_orig = np.loadtxt(filepath, delimiter=' ', skiprows=skiprows)
+    elif '.hdf5' in filepath:
+        with h5py.File(filepath, 'r') as f:
+            wvfms_orig = np.array(f['data'])
+    else:
+        print('AAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHH')
+        raise Exception('File type not recognized')
     wvfms_orig_indices = np.arange(0, len(wvfms_orig), 1)
     nWvfms = len(wvfms_orig)
     print('Finished loading waveforms.')
@@ -63,6 +71,12 @@ def main(filepath):
     extra_signals = 0
     keep_array = np.zeros(len(wvfms), dtype=bool)
     amplitudes = []
+    not_DC_indices = []
+    DC_indices = []     #which waveforms have DC's
+    not_DC_indices2 = []
+    DC_indices2 = []    #where DC is in waveform
+
+    x = 0
     # loop through each waveform and find ones with good correlation between template and itself
     for j, wvfm in tqdm(enumerate(wvfms)):
         i = 0
@@ -75,7 +89,9 @@ def main(filepath):
             signal = wvfm[i:wvfm_end]
             correlation = correlate(signal, template, mode='same')
             i += 5 # for speed skip ahead
-            if np.max(correlation) > correlation_threshold:
+            if (np.max(correlation) > correlation_threshold) and np.any(signal < threshold):
+                DC_indices.append(wvfms_orig_indices[j])
+                DC_indices2.append(i)
                 # keep track of which waveforms have any good correlations
                 if keep_array[j] == True and np.any(signal < threshold):
                     extra_signals += 1
@@ -83,12 +99,24 @@ def main(filepath):
                 else:
                     keep_array[j] = True
                     amplitudes.append(np.min(wvfm))
+                    '''
+                    if (np.max(correlation) < 1500) & (np.max(correlation) > 1200):
+                        not_DC_indices.append(wvfms_orig_indices[j])
+                        not_DC_indices2.append(i)
+                    elif np.max(correlation) > 1500:
+                        DC_indices.append(wvfm_orig_indices[j])
+                        DC_indices2.append(i)
+                    '''
                     #np.save(f'wvfm_{i}.npy', wvfms_orig[wvfms_orig_indices[j]])
                 i += 500 # skip ahead past the current signal and look for more signals in same waveform
+        x +=1
     np.save('amplitudes.npy', amplitudes)
     print(f'Keep {np.sum(keep_array)} waveforms due to good correlation')
     print(f'Found {extra_signals} extra signals in waveforms')
-    
+    print(not_DC_indices)
+    print(DC_indices)
+    print(not_DC_indices2)
+    print(DC_indices2)
     wvfms = wvfms[keep_array] # keep only waveforms with good correlations
         
     # print results
