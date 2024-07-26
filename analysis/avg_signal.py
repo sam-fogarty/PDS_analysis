da02614f60674dea8e64fe6f039dea5e3061b793
Sam Fogarty <samuel.fogarty@colostate.edu>
2024-07-26 16:02:18 -0500

    Cast hdf5 data as float, plot ticks

--- analysis/avg_signal.py
+++ analysis/avg_signal.py
@@ -17,7 +17,7 @@ def main(*filepath):
             data = np.loadtxt(filename, delimiter=' ', skiprows=0)[:]
         elif '.hdf5' in filename:
             with h5py.File(filename, 'r') as f:
-                data = np.array(f['data'])
+                data = np.array(f['data']).astype('float')
         else:
             print('AAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHH')
             raise Exception('File type not recognized')
@@ -25,20 +25,25 @@ def main(*filepath):
         nwvfm = 0
         for wvfm in data:
             wvfm -= np.mean(wvfm[0:200])
-            if np.min(wvfm[500:600]) < -11 and np.min(wvfm[500:600]) > -18:
+            #wvfm_sum += wvfm
+            #nwvfm+=1
+            if np.min(wvfm) < -10 and np.min(wvfm) > -17:
                 wvfm_sum += wvfm
                 nwvfm += 1
         #avg_wvfm = np.sum(data,axis=0)/len(data)
         avg_wvfm = wvfm_sum/nwvfm
-        plt.plot(avg_wvfm, color=colors[i])
+        ticks = np.arange(len(avg_wvfm))
+        plt.plot(ticks[400:1400], avg_wvfm[400:1400], color=colors[i])
+    
+    #np.save('Single_PE_Template_2', avg_wvfm[600:1200])
     plt.xlabel('16ns time ticks')
     plt.ylabel('ADC counts')
     #plt.title('LED Signals HD Module in Dewar at Warm \n (more optimal pulser config)')
     #plt.title('Pulser Noise HD Module in Dewar \n' + f'(channel {ch}, AFE {afe})')
     plt.title('Average 1 p.e. signal in CSU LN2 setup')
     plt.legend()
-    plt.xlim(560, 900)
-    plt.ylim(-15, 3)
+    #plt.xlim(560, 900)
+    #plt.ylim(-15, 3)
     #plt.hist(rms_values)
     #plt.xlabel('ADC RMS')
     #plt.ylabel('Counts')
