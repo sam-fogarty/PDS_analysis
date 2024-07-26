df9ca4331491e1b427b812dde95a791034bdc05f
Sam Fogarty <samuel.fogarty@colostate.edu>
2024-07-26 16:04:00 -0500

    Update compile

--- cpp/compile_new.sh
+++ cpp/compile_new.sh
@@ -1,4 +1,4 @@
-g++ -c daphne.cpp -o daphne.o
+g++ -c daphne_new.cpp -o daphne.o
 ar rcs libdaphne.a daphne.o
 #g++ plot_data.cpp daphne.cpp -o plot_data -I$ROOTSYS/include -L$ROOTSYS/lib -lCore -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lGui -L. -ldaphnei
-g++ collect_data_new.cpp daphne.cpp -o collect_data_new -ldaphne -lhdf5_cpp -lhdf5 #-I$ROOTSYS/include -L$ROOTSYS/lib -lCore -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lGui -L. -ldaphne
+g++ collect_data_new.cpp daphne_new.cpp -o collect_data_new -L/root/PDS_scripts/cpp -ldaphne -I/root/PDS_scripts/cpp/hdf5-1.14.4-3/hdf5/include -L/root/PDS_scripts/cpp/hdf5-1.14.4-3/hdf5/lib -lhdf5 -lhdf5_cpp -I/root/miniconda3/lib -L/root/miniconda3/lib -lz -Wl,-rpath,/root/miniconda3/lib -Wl,-rpath,/root/PDS_scripts/cpp/hdf5-1.14.4-3/hdf5/include/H5Cpp.h  #-I$ROOTSYS/include -L$ROOTSYS/lib -lCore -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lGui -L. -ldaphne
