34046336002f09050662977fa3dadddca0edb055
Sam Fogarty <samuel.fogarty@colostate.edu>
2024-09-05 16:28:38 -0500

    Update persistence

--- analysis/persistence.py
+++ analysis/persistence.py
@@ -12,16 +12,21 @@ def main(filename):
         wvfms = np.genfromtxt(filename, delimiter=' ', max_rows=10000)
     elif '.hdf5' in filename:
         with h5py.File(filename, 'r') as f:
-            wvfms = np.array(f['data'])
+            wvfms = np.array(f['data']).astype('float')
     else:
         print('AAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHH')
         raise Exception('File type not recognized')
+    range_x = np.array([500, 900])
+    wvfms = wvfms[:, range_x[0]:range_x[1]]
+    num_time_steps = range_x[1] - range_x[0]
+    range_x = np.array([0, range_x[1]-range_x[0]])*16
+
     window_size = 1
-    pedestal_range = (350, 450)
+    pedestal_range = (0, 150)
     amplitudes = []
     # make array to store smoothed waveforms
-    wvfm_corr = np.zeros((wvfms.shape[0], wvfms.shape[1]))
-    for eventID in tqdm(range(wvfm_corr.shape[0])):
+    #wvfm_corr = np.zeros((wvfms.shape[0], wvfms.shape[1]))
+    for eventID in tqdm(range(wvfms.shape[0])):
         wvfm = wvfms[eventID]
         mean = np.mean(wvfm[pedestal_range[0]:pedestal_range[1]])
         df = pd.DataFrame({'wvfm': wvfm})
@@ -33,30 +38,32 @@ def main(filename):
     
         wvfm_smoothed = wvfm_smoothed - mean
         amplitudes.append(np.min(wvfm_smoothed))
-        wvfm_corr[eventID, :] = wvfm_smoothed
-    num_waveforms, num_time_steps = wvfm_corr.shape
+        wvfms[eventID, :] = wvfm_smoothed
+    
+    num_waveforms, num_time_steps = wvfms.shape
     #num_time_steps -= window_size
-    flattened_waveforms = wvfm_corr.ravel()
+    wvfms = wvfms.ravel()
     #flattened_waveforms = flattened_waveforms[~np.isnan(flattened_waveforms)]
 
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(16,6))
-    meanADC = np.mean(flattened_waveforms[~np.isnan(flattened_waveforms)])
+    meanADC = np.mean(wvfms[~np.isnan(wvfms)])
+    print(f'meanADC = {meanADC}')
     # Adjust the number of bins and range
     ymax = 10
-    ymin = -70
+    ymin = -40
     num_bins_x = int(num_time_steps/1)
     num_bins_y = int((ymax - ymin)/1)
-    range_x = [0, num_time_steps*16]
+    range_x = np.array([150, 300])*16
     range_y = [ymin, ymax]
     
     plot2d = axes[0].hist2d(
             np.tile(np.arange(num_time_steps)*16, num_waveforms),
-            flattened_waveforms,
-            bins=[num_bins_x, num_bins_y],
+            wvfms,
+            bins=[150, num_bins_y],
             range=[range_x, range_y],
             cmap='viridis',
             vmin=1,
-            vmax=700
+            vmax=800
             )
     #cmap = plt.cm.jet    
     axes[0].set_xlabel(r'Time [ns]')
@@ -69,10 +76,10 @@ def main(filename):
     #colorbar.clim(vmin=vmin, vmax=vmax)
     
     #plt.ylim(-40, 10)
-    axes[0].set_xlim(9000, 11000)
+    #axes[0].set_xlim(11000, 13000)
     #plt.ylim(-50, 30)
 
-    axes[1].hist(amplitudes, bins=75, range=(-75, 10))
+    axes[1].hist(amplitudes, bins=75, range=(-100, 10))
     axes[1].set_xlabel('Waveform Amplitude')
     axes[1].set_xticks([-70,-60,-50,-40,-30,-20,-10,0, 10])
     plt.show()
